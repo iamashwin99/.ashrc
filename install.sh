858538fd02e98962d101b61c602393e2df3ff7d4
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-01-31 15:50:09 +0100

    fix installer

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ if [ -z "$ASHRC" ]; then
 export ASHRC=$HOME/.ashrc
 fi
 
-echo -e "\n###### Ashwin's Customizations ######\n[[ ! -f . $ASHRC/.ashrc ]] || . $ASHRC/.ashrc" >> $rc_file
+echo -e "\n###### Ashwin's Customizations ######\n[[ ! -f  $ASHRC/.ashrc ]] || . $ASHRC/.ashrc" >> $rc_file
