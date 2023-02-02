23d5f426545ac2e521874a631098aa9aabc5f48a
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-02-02 23:57:09 +0100

    add exa install

--- ohmyzsh/plugins/install.sh
+++ ohmyzsh/plugins/install.sh
@@ -27,3 +27,12 @@ curl -fsSL https://cutt.ly/tran-cli | zsh
 # install zoxide
 echo -e "\033[36mInstalling zoxide\033[0m"
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+# install exa
+# if user has sudo rights then install exa with sudo
+if [ -x "$(command -v sudo)" ]; then
+    echo -e "\033[36mInstalling exa\033[0m"
+    sudo apt install exa
+else
+    echo -e "\033[36m No root permission skipping exa\033[0m"
+fi
