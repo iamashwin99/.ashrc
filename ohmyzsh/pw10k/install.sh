6f8e612ba7b8d28633508eb467641e4dd7f02940
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-02-03 00:29:27 +0100

    fix ohmyzsh path

--- ohmyzsh/pw10k/install.sh
+++ ohmyzsh/pw10k/install.sh
@@ -1,15 +1,10 @@
 #!/bin/zsh
 
-# if $ZSH_CUSTOM is not set source ~/.ohmyzshrc and set $ZSH_CUSTOM
-if [ -z "$ZSH_CUSTOM" ]; then
-    . ~/.ohmyzshrc
-fi
-
 
 # Install pwerlevel10k
 # if folder $ZSH_CUSTOM/themes/powerlevel10k is not present then clone  powerlevel10k and link p10k.zsh to ~/.p10k.zsh
-if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+if [ ! -d "~/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     echo -e "\033[36mInstalling powerlevel10k\033[0m"
-    git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+    git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
     ln -s $ASHRC/ohmyzsh/p10k.zsh ~/.p10k.zsh
 fi
