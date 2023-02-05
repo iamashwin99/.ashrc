b1c726a35c255c7a2cba4fe9f14135c1283afc68
Ashwin Kumar <46030335+iamashwin99@users.noreply.github.com>
2023-02-05 13:19:43 +0100

    Update install-meslo-fonts.sh

--- ohmyzsh/pw10k/install-meslo-fonts.sh
+++ ohmyzsh/pw10k/install-meslo-fonts.sh
@@ -3,3 +3,5 @@ curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf --output ~/.fonts/'MesloLGS NF Bold.ttf'
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf --output ~/.fonts/'MesloLGS NF Italic.ttf'
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf --output ~/.fonts/'MesloLGS NF Bold Italic.ttf'
+
+# TODO install to system
