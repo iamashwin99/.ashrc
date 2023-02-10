0507db11f8f3c6e6bdf4bbeae889152e2cba94d2
iamashwin <ashwin-kumar.karnad@mpsd.mpg.de>
2023-02-10 18:09:23 +0100

    add exa

--- ohmyzsh/plugins/install.sh
+++ ohmyzsh/plugins/install.sh
@@ -24,10 +24,20 @@ echo -e "\033[36mInstalling zoxide\033[0m"
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
 # install exa
-# if user has sudo rights then install exa with sudo
-if [ -x "$(command -v sudo)" ]; then
+# download https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
+# unzip exa-linux-x86_64-v0.10.1.zip
+# mv exa-linux-x86_64/bin/exa $HOME/.local/bin/exa
+# mv exa-linux-x86_64/man/* /home/karnada/.local/share/man/man1
+# rm -rf exa-linux-x86_64
+
+if [ ! -x $HOME/.local/bin/exa ]; then
     echo -e "\033[36mInstalling exa\033[0m"
-    sudo apt install exa
+    wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip -O /tmp/exa.zip
+    unzip /tmp/exa.zip -d /tmp/exa
+    mv /tmp/exa/bin/exa $HOME/.local/bin/exa
+    mv /tmp/exa/man/* $HOME/.local/share/man/man1/
+    rm -rf /tmp/exa
+    rm /tmp/exa.zip
 else
-    echo -e "\033[36m No root permission skipping exa\033[0m"
+    echo -e "\033[36mexa is already installed\033[0m"
 fi
