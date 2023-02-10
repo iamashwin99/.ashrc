363ac53b4f089eee1a65f647460099286892a762
iamashwin <ashwin-kumar.karnad@mpsd.mpg.de>
2023-02-10 10:11:29 +0100

    update zoxide installer

--- zoxd/install.sh
+++ zoxd/install.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Install zoxd
-if [ ! -x "$HOME/.zoxd" ]; then
+if [ ! -x "$HOME/.local/bin/zoxide" ]; then
     echo -e "\033[36mInstalling zoxd\033[0m"
     curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
     touch $HOME/.zoxd
